80239f8842337c178f803076395755005a5add3d
Cledson <cledson1996@gmail.com>
2022-12-23 11:42:26 -0300

    shorten_ok

--- src/controllers/shorten.controller.js
+++ src/controllers/shorten.controller.js
@@ -6,11 +6,35 @@ export async function shorten(req, res) {
 
     const url = req.body.url
 
-    let user
+    let userId = res.locals.user
+
 
     if (!url) {
-        res.status(401).send("body invalido!!!")
+        res.status(422).send("body invalido!!!")
     }
-    console.log(res.locals.user)
+
+    let model = nanoid(10).toLowerCase()
+
+    try {
+
+        const { rows } = await connection.query("SELECT * FROM shorten WHERE url=$1;", [url]);
+
+        if (rows.length > 0) {
+            res.status(422).send(`Esta url ja esta encurtada com a shortUrl= ${rows[0].shorturl}`);
+            return
+        }
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+
+    try {
+        await connection.query("INSERT INTO shorten (url,shorturl,userid) VALUES ($1, $2, $3);", [url, model, userId]);
+        res.status(201).send({
+            "shortUrl": model
+        });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 
 }
